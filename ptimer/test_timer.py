5c6789b9f9e3e3d04e0d69d698d44e9230a24c47
salomaestro <chris10an.salomonsen@gmail.com>
2023-09-03 17:26:52 +0200

    extra tests

--- ptimer/test_timer.py
+++ ptimer/test_timer.py
@@ -7,3 +7,18 @@ def test_ctx_mgr():
         sleep(1)
     assert t.elapsed >= 1
     assert t.name is None
+
+
+def test_explainer():
+    with Timer("test", explainer="Time taken") as t:
+        pass
+    assert t.name == "test"
+    assert t.explainer == "Time taken"
+
+
+def test_fmt():
+    def fmt(x): return f'{x} seconds'
+    with Timer(time_fmt=fmt) as t:
+        pass
+
+    assert t.time_fmt == fmt
